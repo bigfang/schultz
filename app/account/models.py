567f5a67f1af2543645512ab710e3fcf191e3d60
bigfang <bitair@gmail.com>
2025-04-27 21:56:55 +0800

    增加基本app: account, cli

---
+++ app/account/models.py
@@ -0,0 +1,26 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from lib.models import DeleteMixin, TimestampMixin
+
+
+class User(AbstractUser, TimestampMixin, DeleteMixin):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False, verbose_name='ID')
+    first_name = None
+    last_name = None
+
+    REQUIRED_FIELDS: list[str] = ['email']
+
+    class Meta:
+        db_table = 'auth_user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_full_name(self) -> None:
+        pass
+
+    def get_short_name(self) -> None:
+        pass
